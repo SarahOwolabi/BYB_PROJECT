7ab0c8bf51c5d6ad45ff01701554b65bf656ec26
SARAH Y OWOLABI <164075073+SarahOwolabi@users.noreply.github.com>
2024-03-29 00:21:31 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+"""
+#Pseudocode
+request input from user and store in variable called username
+request for username and print as output in terminal
+    then print username.
+"""""
+
+username= input("Enter your name:")
+print(username)
+
+#print("Hello+username")
+
+"""#pseudocode2
+request user age and save as variable
+then store age as an integer
+print input """
+
+userage= input("Enter your age:")
+print(userage)
+print(int(userage))
+
+
+print("Hello World!")
